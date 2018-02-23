e1efa834874b35cb370a1d26548ab031d5589b1a
Harkirat Singh <billharki@hotmail.com>
2018-02-23 08:14:18 +0530

    Initial Commit

---
+++ Gurbani VeecharTests/Gurbani_VeecharTests.swift
@@ -0,0 +1,36 @@
+//
+//  Gurbani_VeecharTests.swift
+//  Gurbani VeecharTests
+//
+//  Created by Harkirat Singh on 23/02/18.
+//  Copyright © 2018 Harkirat Singh. All rights reserved.
+//
+
+import XCTest
+@testable import Gurbani_Veechar
+
+class Gurbani_VeecharTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
